61b3307512f6868291e1c08d18e0d417b73cbf40
Pim Bongaerts <pimbongaerts@gmail.com>
2025-08-21 11:32:01 +1200

    Make sam optional

--- src/substrata/segmentation.py
+++ src/substrata/segmentation.py
@@ -9,8 +9,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from PIL import Image
-from sam2.build_sam import build_sam2
-from sam2.sam2_image_predictor import SAM2ImagePredictor
 from tqdm import tqdm
 from joblib import Parallel, delayed
 
